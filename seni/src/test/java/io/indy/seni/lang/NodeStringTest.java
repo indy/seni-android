2faedff2a06ab2fba4dcc3627cb05038cb793ed1
Inderjit Gill <email@indy.io>
2013-11-05 12:53:44 +0000

    add: NodeStringTest

---
+++ seni/src/test/java/io/indy/seni/lang/NodeStringTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 Inderjit Gill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.indy.seni.lang;
+
+import java.util.List;
+
+import org.junit.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class NodeStringTest {
+
+    @Test
+    public void testNodeString() {
+        NodeString n = new NodeString("cons");
+
+        assertThat(n.getType()).isEqualTo(Node.Type.STRING);
+        assertThat(n.getString()).isEqualTo("cons");
+    }
+
+    @Test
+    public void testEq() {
+        NodeString n = new NodeString("cons");
+
+        assertThat(n.eq(n)).isTrue();
+        assertThat(n.eq(new NodeString("cons"))).isTrue();
+
+        assertThat(n.eq(new NodeString("car"))).isFalse();
+    }
+
+    @Test
+    public void testScribe() {
+        NodeString n = new NodeString("cons");
+        assertThat(n.scribe()).isEqualTo("cons");
+    }
+}

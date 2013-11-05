457840f4d2591507acd5005f004f16512a3cfe71
Inderjit Gill <email@indy.io>
2013-11-05 09:53:11 +0000

    add: NodeNameTest

---
+++ seni/src/test/java/io/indy/seni/lang/NodeNameTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2013 Inderjit Gill
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.indy.seni.lang;
+
+import java.util.List;
+
+import org.junit.Test;
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class NodeNameTest {
+
+    @Test
+    public void testNodeName() {
+        NodeName n = new NodeName("cons");
+
+        assertThat(n.getType()).isEqualTo(Node.Type.NAME);
+        assertThat(n.getName()).isEqualTo("cons");
+    }
+
+    @Test
+    public void testEq() {
+        NodeName n = new NodeName("cons");
+
+        assertThat(n.eq(n)).isTrue();
+        assertThat(n.eq(new NodeName("cons"))).isTrue();
+
+        assertThat(n.eq(new NodeName("car"))).isFalse();
+    }
+
+    @Test
+    public void testScribe() {
+        NodeName n = new NodeName("cons");
+        assertThat(n.scribe()).isEqualTo("cons");
+    }
+}

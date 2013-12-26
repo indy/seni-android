d88a8fd7ae1b10f89c6885e330883e935e88bfe0
Inderjit Gill <email@indy.io>
2013-12-26 16:09:00 +0000

    update: latest version of gradle

--- seni/src/main/java/io/indy/seni/AppConfig.java
+++ seni/src/main/java/io/indy/seni/AppConfig.java
@@ -20,7 +20,9 @@ public final class AppConfig {
 
     public static final boolean DEBUG = BuildConfig.DEBUG;
 
-    public static final String VERSION = BuildConfig.VERSION;
-    public static final String BUILD_CODE = BuildConfig.BUILD_TIME + "-" + BuildConfig.GIT_SHA;
+    //public static final String VERSION = BuildConfig.VERSION;
+    //public static final String BUILD_CODE = BuildConfig.BUILD_TIME + "-" + BuildConfig.GIT_SHA;
+    public static final String VERSION = "0.0.1";
+    public static final String BUILD_CODE = "." + "-" + ".";
 
 }

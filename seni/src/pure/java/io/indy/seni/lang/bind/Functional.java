1f46f237079226d51de431e8d492ff68eb25687c
Inderjit Gill <email@indy.io>
2014-01-28 14:27:51 +0000

    updated copyright notices

--- seni/src/pure/java/io/indy/seni/lang/bind/Functional.java
+++ seni/src/pure/java/io/indy/seni/lang/bind/Functional.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Inderjit Gill
+ * Copyright 2014 Inderjit Gill
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
